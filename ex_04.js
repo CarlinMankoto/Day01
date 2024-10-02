a3571bb7fec74f5b46f6bee4437f5609c3640892
CarlinMankoto <carlin.mankoto@epitech.digital>
2024-10-02 16:16:14 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof undeclaredVariable)
+displayThisText(typeof true)
+displayThisText(typeof 93)
+displayThisText(typeof 93n)
+displayThisText(typeof Symbol())
+displayThisText(typeof {})
+displayThisText(typeof $)
